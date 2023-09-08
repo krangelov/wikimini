09a6bf1e710c70624f1ed1cf62f7042b7122e86c
Aarne Ranta <aarne@chalmers.se>
2023-09-08 08:36:00 +0200

    missing funs per sentence

--- test_wiki.py
+++ test_wiki.py
@@ -9,13 +9,30 @@ TREE_FILE = 'data/all_trees.txt'
 
 gr = pgf.readPGF(PGF_FILE)
 
+def funs_in_tree(tree):
+    funs = []
+    f, xs = tree.unpack()
+    funs.append(f)
+    for x in xs:
+        for f in funs_in_tree(x):
+            funs.append(f)
+    return funs
+
+
 with open(TREE_FILE) as trees:
+    missing = {lang: {} for lang in gr.languages}
     for tree in trees:
         try:
             tree = pgf.readExpr(tree)
             print(tree)
-            for lang in gr.languages.values():
+            for name, lang in gr.languages.items():
                 print(lang.linearize(tree))
+                for f in funs_in_tree(tree):
+                    if not lang.hasLinearization(f):
+                        missing[name][f] = missing[name].get(f, 0) + 1
         except:
             print(tree)
-        
+    print(missing)
+    for name, funs in missing.items():
+        print(name, len(funs))
+
